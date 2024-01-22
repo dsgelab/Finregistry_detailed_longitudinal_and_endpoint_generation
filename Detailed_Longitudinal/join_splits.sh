f92c344143dc51fd82910258235b0613625edf00
ferroteo <ferro.teo98@gmail.com>
2024-01-22 14:27:38 +0200

    fix join_splits script

--- Detailed_Longitudinal/join_splits.sh
+++ Detailed_Longitudinal/join_splits.sh
@@ -1,6 +1,7 @@
 
-head -n 1 Hilmo_1994_1995.csv > detailed_longitudinal_fromsplits.csv  
+DL_FILE=/data/processed_data/detailed_longitudinal/R10/service_sector/detailed_longitudinal_2024_19_01.csv
+head -n 1 /data/processed_data/detailed_longitudinal/R10/service_sector/splits/Cancer.csv > $DL_FILE  
 for file in /data/processed_data/detailed_longitudinal/R10/service_sector/splits/*; do
     # Append contents, excluding the header
-    tail -n +2 "$file" >> detailed_longitudinal_fromsplits.csv    
+    tail -n +2 "$file" >> $DL_FILE    
 done
