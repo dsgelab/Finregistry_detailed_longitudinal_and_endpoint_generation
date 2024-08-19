1d4353b5d2b110bee78c592b04e26d8d4991f393
ferroteo <ferro.teo98@gmail.com>
2024-08-19 08:23:52 +0300

    update file

--- Detailed_Longitudinal/join_splits.sh
+++ Detailed_Longitudinal/join_splits.sh
@@ -1,7 +1,11 @@
 
-DL_FILE=/data/processed_data/detailed_longitudinal/R10/service_sector/detailed_longitudinal_2024_19_01.csv
-head -n 1 /data/processed_data/detailed_longitudinal/R10/service_sector/splits/Cancer.csv > $DL_FILE  
-for file in /data/processed_data/detailed_longitudinal/R10/service_sector/splits/*; do
-    # Append contents, excluding the header
+# define the output directory and the filename to be used
+OutDir=/data/processed_data/detailed_longitudinal/R10/backup
+DL_FILE=$OutDir/detailed_longitudinal_2024_08_18.csv
+
+#prepare header of file
+head -n 1 $OutDir/splits_2024_08_18/Cancer.csv > $DL_FILE  
+# Append all files, excluding the header
+for file in $OutDir/splits_2024_08_18/*; do
     tail -n +2 "$file" >> $DL_FILE    
 done
